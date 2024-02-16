d16631d91b6463cde3f2e05e0bcf1fd20d359623
Creastv <creastv@gail.com>
2024-02-16 11:10:52 +0100

    update

--- templates-parts/header/header-cat.php
+++ templates-parts/header/header-cat.php
@@ -17,11 +17,19 @@ if ($query->have_posts()) {
     $counter = 0;
     while ($query->have_posts()) {
         $query->the_post();
+        // $baner['link'] = '';
+        // $baner['zdjecie'] = '';
+        // $baner['tytul'] = '';
+        // $baner['opis'] = '';
+        // $baner2['tytul'] = '';
+        // $baner2['opis'] = '';
+        // $baner2['zdjecie'] = '';
         if ($baner['link']) :
             $baner_url = $baner['link']['url'];
             $baner_title = $baner['link']['title'];
             $baner_target = $baner['link']['target'] ? $baner['link']['target'] : '_self';
         endif;
+        $baner2['link'] = null;
         if ($baner2['link']) :
             $baner2_url = $baner2['link']['url'];
             $baner2_title = $baner2['link']['title'];
@@ -44,43 +52,46 @@ if ($query->have_posts()) {
             the_post_thumbnail('post-futured');
             echo '</div>';
             echo '</div>';
-            echo '<div class="banner">';
+            if ($baner) :
+                echo '<div class="banner">';
 
-            echo '<div class="banner__content">';
-            echo '<p class="h4">';
-            echo $baner['tytul'];
-            echo '</p>';
-            echo '<p>';
-            echo $baner['opis'];
-            echo '</p>';
-            if ($baner['link']) :
-                echo '<a class="btn" href="' . esc_url($baner_url) . '" target=" ' . esc_attr($baner_target) . ' " > ' . esc_html($baner_title) . '</a>';
+                echo '<div class="banner__content">';
+                echo '<p class="h4">';
+                echo  $baner['tytul'] ? $baner['tytul'] : false;
+                echo '</p>';
+                echo '<p>';
+                echo $baner['opis'] ? $baner['opis'] : false;
+                echo '</p>';
+                if ($baner['link']) :
+                    echo '<a class="btn" href="' . esc_url($baner_url) . '" target=" ' . esc_attr($baner_target) . ' " > ' . esc_html($baner_title) . '</a>';
+                endif;
+                echo '</div>';
+                echo '<div class="banner__img">';
+                echo $baner['zdjecie'] ? wp_get_attachment_image($baner['zdjecie'], 'post-futured') : false;
+                echo '</div>';
+                echo '</div>';
             endif;
-            echo '</div>';
-            echo '<div class="banner__img">';
-            echo wp_get_attachment_image($baner['zdjecie'], 'post-futured');
-            echo '</div>';
-            echo '</div>';
             echo '</div>';
         } else {
             echo '<div class="col">';
-            echo '<div class="banner">';
-
-            echo '<div class="banner__content">';
-            echo '<p class="h4">';
-            echo $baner2['tytul'];
-            echo '</p>';
-            echo '<p>';
-            echo $baner2['opis'];
-            echo '</p>';
-            if ($baner['link']) :
-                echo '<a class="btn" href="' . esc_url($baner2_url) . '" target=" ' . esc_attr($baner2_target) . ' " > ' . esc_html($baner2_title) . '</a>';
+            if ($baner) :
+                echo '<div class="banner">';
+                echo '<div class="banner__content">';
+                echo '<p class="h4">';
+                echo $baner2['tytul'] ? $baner2['tytul'] : false;
+                echo '</p>';
+                echo '<p>';
+                echo $baner2['opis'] ? $baner2['opis'] : false;
+                echo '</p>';
+                if ($baner['link']) :
+                    echo '<a class="btn" href="' . esc_url($baner2_url) . '" target=" ' . esc_attr($baner2_target) . ' " > ' . esc_html($baner2_title) . '</a>';
+                endif;
+                echo '</div>';
+                echo '<div class="banner__img">';
+                echo $baner2['zdjecie'] ? wp_get_attachment_image($baner2['zdjecie'], 'post-futured') : false;
+                echo '</div>';
+                echo '</div>';
             endif;
-            echo '</div>';
-            echo '<div class="banner__img">';
-            echo wp_get_attachment_image($baner2['zdjecie'], 'post-futured');
-            echo '</div>';
-            echo '</div>';
             echo '<div class="post">';
             echo '<div class="post__content">';
             echo '<h2>';
