f57f2a864423dc6d924b31c37bc3632df7160983
Creastv <creastv@gail.com>
2024-02-16 07:50:43 +0100

    update

--- func/woocommerce.php
+++ func/woocommerce.php
@@ -121,7 +121,7 @@ jQuery(function($) {
 }
 
 // gallery
-add_theme_support('wc-product-gallery-zoom');
+// add_theme_support('wc-product-gallery-zoom');
 add_theme_support('wc-product-gallery-lightbox');
 add_theme_support('wc-product-gallery-slider');
 
