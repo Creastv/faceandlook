3e0632fc6aa88fae489e92ad4a9ec7c9025cd29d
Creastv <creastv@gail.com>
2024-02-20 16:39:34 +0100

    update

--- blocks/posts-grid/post-last.php
+++ blocks/posts-grid/post-last.php
@@ -14,27 +14,27 @@ $query_posts_one = new WP_Query($po);
             $query_posts_one->the_post();
             $term_list = wp_get_post_terms(get_the_id(), 'category', array("fields" => "names"));
         ?>
-            <div class="col">
-                <div class="post">
-                    <div class="post__img">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('post-futured'); ?>
-                        </a>
-                    </div>
-                    <div class="post__content">
+        <div class="col">
+            <div class="post">
+                <div class="post__img">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('medium'); ?>
+                    </a>
+                </div>
+                <div class="post__content">
 
-                        <div class="entry-term">
-                            <span> <?php echo $term_list[0]; ?></span>
-                        </div>
-                        <h2>
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </h2>
-                        <?php the_excerpt(); ?>
+                    <div class="entry-term">
+                        <span> <?php echo $term_list[0]; ?></span>
                     </div>
+                    <h2>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </h2>
+                    <?php the_excerpt(); ?>
                 </div>
             </div>
+        </div>
         <?php }
         wp_reset_postdata(); ?>
     </div>
