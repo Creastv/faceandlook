75864e6a1aa6d6541053c8ad5e3a5bd763139254
Creastv <creastv@gail.com>
2024-02-20 13:12:14 +0100

    update

--- templates-parts/header/header-category.php
+++ templates-parts/header/header-category.php
@@ -12,40 +12,40 @@ $query_cat = new WP_Query($args);
 
 
 <?php if ($query_cat->have_posts()) { ?>
-    <div class="header-cat-wrap">
-        <div class="header-cat-slider">
-            <div class="swiper mySwiper">
-                <div class="swiper-wrapper">
-                    <?php while ($query_cat->have_posts()) {
+<div class="header-cat-wrap">
+    <div class="header-cat-slider">
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <?php while ($query_cat->have_posts()) {
                         $query_cat->the_post();
                     ?>
-                        <div class="swiper-slide">
-                            <div class="post">
-                                <div class="post__img">
-                                    <?php the_post_thumbnail(''); ?>
-                                </div>
-                                <div class="post__content">
-                                    <h2>
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php the_title(); ?>
-                                        </a>
-                                    </h2>
-                                    <?php the_excerpt(); ?>
-                                    <a href="<?php the_permalink(); ?>" class="btn-revers">Czytaj wiecej</a>
-                                </div>
-                            </div>
+                <div class="swiper-slide">
+                    <div class="post">
+                        <div class="post__img">
+                            <?php the_post_thumbnail(''); ?>
                         </div>
-                    <?php  }
-                    wp_reset_postdata(); ?>
+                        <div class="post__content">
+                            <h2>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+                            <?php // the_excerpt(); ?>
+                            <a href="<?php the_permalink(); ?>" class="btn-revers">Czytaj wiecej</a>
+                        </div>
+                    </div>
                 </div>
-
+                <?php  }
+                    wp_reset_postdata(); ?>
             </div>
-        </div>
-        <div class="container">
-            <div class="swiper-scrollbar"></div>
-        </div>
 
+        </div>
     </div>
+    <div class="container">
+        <div class="swiper-scrollbar"></div>
+    </div>
+
+</div>
 
 <?php } ?>
 
@@ -57,30 +57,30 @@ $query_cat = new WP_Query($args);
 
 <!-- Initialize Swiper -->
 <script>
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: "auto",
-        spaceBetween: 10,
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
 
-        scrollbar: {
-            el: ".swiper-scrollbar",
-            hide: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        400: {
+            // slidesPerView: 2,
+            spaceBetween: 20,
+
         },
-        autoplay: {
-            delay: 5500,
-            disableOnInteraction: false,
-        },
-        breakpoints: {
-            400: {
-                // slidesPerView: 2,
-                spaceBetween: 20,
-
-            },
-            640: {
-                // slidesPerView: 2,
-                spaceBetween: 20,
-
-            }
+        640: {
+            // slidesPerView: 2,
+            spaceBetween: 20,
 
         }
-    });
+
+    }
+});
 </script>
