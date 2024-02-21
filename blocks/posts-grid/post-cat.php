36b6b1ccec4c6cca1545f5246b55c4164e337f08
Creastv <creastv@gail.com>
2024-02-21 12:46:55 +0100

    update

---
+++ blocks/posts-grid/post-cat.php
@@ -0,0 +1,43 @@
+<?php
+$kategoria = get_field('kategorie');
+$po = array(
+    'post_type' => 'post',
+    'posts_per_page' => 4,
+    'ignore_sticky_posts' => 1,
+    'cat' => $kategoria
+);
+$query_posts_one = new WP_Query($po);
+
+?>
+<div class="posts-grid">
+    <div class="posts-grid__wrap posts-grid__wrap--style-one">
+
+        <?php while ($query_posts_one->have_posts()) {
+            $query_posts_one->the_post();
+            $term_list = wp_get_post_terms(get_the_id(), 'category', array("fields" => "names"));
+        ?>
+            <div class="col">
+                <div class="post">
+                    <div class="post__img">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('wc-product'); ?>
+                        </a>
+                    </div>
+                    <div class="post__content">
+
+                        <div class="entry-term">
+                            <span> <?php echo $term_list[0]; ?></span>
+                        </div>
+                        <h2 class="h3">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_title(); ?>
+                            </a>
+                        </h2>
+                        <?php the_excerpt(); ?>
+                    </div>
+                </div>
+            </div>
+        <?php }
+        wp_reset_postdata(); ?>
+    </div>
+</div>
--- blocks/posts-grid/posts-grid.php
+++ blocks/posts-grid/posts-grid.php
@@ -15,5 +15,9 @@ $posts = get_field('wyswietlane_posty_po');
 
 <?php }
 if ($type == 1) { ?>
+<?php if ($style == 'Styl 1') { ?>
 <?php get_template_part('blocks/posts-grid/post-grid-cat'); ?>
+<?php } else { ?>
+<?php get_template_part('blocks/posts-grid/post-cat'); ?>
+<?php } ?>
 <?php } ?>
