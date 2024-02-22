0fb17967a9888705e1d252507afc1c2581157c55
Creastv <creastv@gail.com>
2024-02-22 16:52:07 +0100

    update

---
+++ blocks/wc-category/wc-category.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".wc-cats", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      }
+    }
+  });
+§
---
+++ blocks/wc-category/wc-category.php
@@ -0,0 +1,32 @@
+<?php
+$terms = get_field('kategorie_produktow');
+
+if ($terms) : ?>
+    <div class="b-wc-cats">
+        <div class="swiper new-slider wc-cats">
+            <div class="swiper-wrapper">
+                <?php foreach ($terms as $term) :
+                    $term_name = $term->name;  // Term name
+                    $term_slug = $term->slug;  // Term slug
+                    $term_id = $term->term_id;
+
+                    $cat_thumb_id = get_woocommerce_term_meta($term->term_id, 'thumbnail_id', true);
+                    $shop_catalog_img = wp_get_attachment_image_src($cat_thumb_id, 'thumbnail');
+                ?>
+                    <div class="swiper-slide">
+                        <div class="b-wc-cats-item">
+                            <div class="img">
+                                <?php if ($shop_catalog_img) : ?>
+                                    <img src="<?php echo $shop_catalog_img[0]; ?>" alt="<?php echo esc_html($term_name); ?>">
+                                <?php else : ?>
+                                    <span>test</span>
+                                <?php endif; ?>
+                            </div>
+                            <span><?php echo esc_html($term_name); ?></span>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
