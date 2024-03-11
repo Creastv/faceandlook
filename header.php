8cf85aa841057eebaf530ee472cd43de084fbbdd
Creastv <creastv@gail.com>
2024-03-11 16:01:10 +0100

    update

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <meta name="google-site-verification" content="EecTcvdM0xO2hTF18KPt-aHk1WlgoacxMqV7DU-UW0Y" />
     <meta name="theme-color" content="#fff">
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,9 +10,7 @@
     <title><?php wp_title('|', true, 'right'); ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&family=Sorts+Mill+Goudy&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&family=Sorts+Mill+Goudy&display=swap" rel="stylesheet">
     <script src="https://integrations.etrusted.com/applications/widget.js/v2" defer async></script>
     <?php wp_head(); ?>
 </head>
