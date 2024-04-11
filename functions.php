75a5270045e0cf616d13e02f55c7ae3c8b9057c0
Creastv <creastv@gail.com>
2024-04-11 11:47:19 +0200

    update

--- functions.php
+++ functions.php
@@ -256,3 +256,29 @@ function remove_admin_bar()
 		show_admin_bar(false);
 	}
 }
+
+
+
+/**
+ * Reload after Consent. 
+ * Only necessary for incomplete integrations, or plugins which handle consent serverside
+ */
+function cmplz_reload_after_consent()
+{
+?>
+	<script>
+		document.addEventListener('cmplz_status_change', function(e) {
+			if (e.detail.category === 'marketing' && e.detail.value === 'allow') {
+				location.reload();
+			}
+		});
+
+		document.addEventListener('cmplz_status_change_service', function(e) {
+			if (e.detail.value) {
+				location.reload();
+			}
+		});
+	</script>
+<?php
+}
+add_action('wp_footer', 'cmplz_reload_after_consent');
